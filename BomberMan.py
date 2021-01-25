44adf46f6a823df279452fb813281c5d1f8e1247
Sm0keHokage <74914263+Sm0keHokage@users.noreply.github.com>
2021-01-25 16:08:16 +0500

    Add files via upload

--- BomberMan.py
+++ BomberMan.py
@@ -22,7 +22,6 @@ TIMEOUT = 0
 
 
 def terminate():
-    print('ds')
     pygame.quit()
     sys.exit()
 
@@ -49,6 +48,18 @@ def restart():
     pygame.mixer.music.set_volume(0.02)
     pygame.mixer.music.play(-1)
     main()
+
+
+def win():
+    all_sprites = pygame.sprite.Group()
+    while True:
+        x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+        create_particles((x, y))
+        all_sprites.update()
+        screen.fill((0, 0, 0))
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 def start_screen():
@@ -329,6 +340,51 @@ ANIMATION_RIGHT_ENEMY_TWO = [('pictures/e_4_right.png', 1)]
 ANIMATION_LEFT_ENEMY_TWO = [('pictures/e_4_left.png', 1)]
 ANIMATION_UP_ENEMY_TWO = [('pictures/e_4_up.png', 1)]
 ANIMATION_DOWN_ENEMY_TWO = [('pictures/e_4_down.png', 1)]
+
+# для отслеживания улетевших частиц
+# удобно использовать пересечение прямоугольников
+screen_rect = (0, 0, WIDTH, HEIGHT)
+GRAVITY = 10
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        sprite.Sprite.__init__(self)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    # количество создаваемых частиц
+    particle_count = 20
+    # возможные скорости
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
 
 
 class Enemy_Two(pygame.sprite.Sprite):
